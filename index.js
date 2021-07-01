d12640e93ebae3f8a060f65d070c9eefec0f7213
26eanderson <26eanderson@sfedstechnology.org>
2021-07-01 11:10:07 -0500

    done

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//creatign variables
 var subject = [""]
 var title = [""]
 var time = [""]
@@ -6,8 +7,10 @@ var dueDate = [""]
 var dueDay = [""]
 var day = [""]
 var month = [""]
-var counter = 0
+//var counter = 0
+// function when submit is clicked
 $("#submit").on("click",function(){
+   // reasighning variables
     dueDay = ($("#dueDay").val());
     dueDate = ($("#dueDate").val());
     type = ($("#type").val());
@@ -16,11 +19,15 @@ $("#submit").on("click",function(){
     subject = ($("#subject").val()); 
     day = ($("#day").val());
     month = ($("#month").val());  
+    //creatign new div
     var newDiv = document.createElement("div");
+    // assighnign varibles to new div
     newDiv.textContent =  (subject + " " + title+ ". " + type + " that will take " + time + " minutes to finish. Due "  + dueDay + "  " + month + " the " + day);
     //document.getElementById("newDiv").innerHTML //= "<div>" + " "+ $("#subject").val() + " " + $("#name").val() + " " + $("#type").val() + " " + $("#time").val() + " minutes to finish. Due "  + $("#dueDay").val() + " "+ "</div>"
-        emptyDiv.append(newDiv)
-      counter ++
+      //prining newDiv benith emptyDiv  
+    emptyDiv.append(newDiv)
+      //counter ++
+      //Checking to make sure the correct varibles are going to be displayed
       console.log(subject[counter])
       console.log(time[counter])
       console.log(type[counter])
